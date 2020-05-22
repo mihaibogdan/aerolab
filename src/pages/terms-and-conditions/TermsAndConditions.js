c65699cc51a0e893d5d094c6f077298d95113a3b
Bogdan Mihai <mihai.bogdan94@gmail.com>
2020-05-23 00:50:05 +0300

    Fixed eslint error

--- src/pages/terms-and-conditions/TermsAndConditions.js
+++ src/pages/terms-and-conditions/TermsAndConditions.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
-import NavbarInner from '../../components/navbar_inner';
 import Footer from 'components/footer';
+import NavbarInner from 'components/navbar_inner';
 import { Heading2, Heading4, Paragraph } from 'assets/styles/typography';
 
 import { Section, Container } from './style';
