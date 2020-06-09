ac02be4369a5993d997496dd9dda298a998c9e18
Bogdan Mihai <mihai.bogdan94@gmail.com>
2020-06-10 00:28:08 +0300

    Compression test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, 'public');
 const port = process.env.PORT || 80;
 
 app.use(express.static(publicPath));
-app.use(compression({ filter: shouldCompress }));
+app.use(compression());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
@@ -15,13 +15,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log('Server is up!');
 });
-
-function shouldCompress(req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false;
-  }
-
-  // fallback to standard filter function
-  return compression.filter(req, res);
-}
